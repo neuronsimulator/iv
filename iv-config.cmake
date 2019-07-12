dd79ebd5d046e3f67db91e6130c51787cbf99373
Omar Awile <omar.awile@epfl.ch>
2019-07-12 16:18:09 +0200

    Added variables to easily find iv installation paths
    - Added into iv-config.cmake two find_path statements to easily publish
      lib and include paths through cmake find_package mechanism.

--- iv-config.cmake
+++ iv-config.cmake
@@ -1,4 +1,8 @@
 # iv-config.cmake - package configuration file
 
 get_filename_component(CONFIG_PATH "${CMAKE_CURRENT_LIST_FILE}" PATH)
+
+find_path(iv_INCLUDE "ivversion.h" HINTS "${CONFIG_PATH}/../../../include")
+find_path(iv_LIB NAMES libinterviews.a libinterviews.so HINTS "${CONFIG_PATH}/../../../lib")
+
 include(${CONFIG_PATH}/iv.cmake)
