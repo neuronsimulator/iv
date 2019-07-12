79e979bb9b055aa2751dfa4ffa7a9a02f34f4f55
Omar Awile <omar.awile@epfl.ch>
2019-07-12 16:29:42 +0200

    Added dylib extension to find the file on mac too

--- iv-config.cmake
+++ iv-config.cmake
@@ -3,6 +3,6 @@
 get_filename_component(CONFIG_PATH "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
 find_path(iv_INCLUDE "ivversion.h" HINTS "${CONFIG_PATH}/../../../include")
-find_path(iv_LIB NAMES libinterviews.a libinterviews.so HINTS "${CONFIG_PATH}/../../../lib")
+find_path(iv_LIB NAMES libinterviews.a libinterviews.so libinterviews.dylib HINTS "${CONFIG_PATH}/../../../lib")
 
 include(${CONFIG_PATH}/iv.cmake)
